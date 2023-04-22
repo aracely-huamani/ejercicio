2f70c47be02e45ba12d1350cd336dbecb05345a7
aracely-huamani <102335298+aracely-huamani@users.noreply.github.com>
2023-04-22 18:07:50 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio/ejercicio/ModalPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ejercicio
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModalPage : ContentPage
+    {
+        public ModalPage()
+        {
+            InitializeComponent();
+            btnClosePopup.Clicked += async (sender, e) =>
+            {
+                await Navigation.PopModalAsync();
+            };
+
+        }
+    }
+}
